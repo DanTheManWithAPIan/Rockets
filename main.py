ed7b0242ec5cc4c05d0e8157f28235ab3dc78151
Daniel Huber <djhuber@iastate.edu>
2022-10-09 01:54:45 -0500

    Added falling space rocks workingo on adding loop logic to make it infinite

--- main.py
+++ main.py
@@ -28,13 +28,13 @@ laser_rescaled = pygame.transform.scale(laser, (30, 40))
 rock = pygame.image.load("rock.png")
 rock_rescaled = pygame.transform.scale(rock, (100, 105))
 
-
 class Rocket:
-    def __init__(self, rocket_x=200, rocket_y=550, rocket_vel=10, hit_points=1):
+    def __init__(self, rocket_x=200, rocket_y=550, rocket_vel=10, rocket_image=rocket_rescaled):
+        self.image = rocket_rescaled.get_rect().copy()
         self.rocket_x = rocket_x
         self.rocket_y = rocket_y
         self.rocket_vel = rocket_vel
-        self.hit_points = hit_points
+        self.rocket_image = rocket_image
 
     def rocket_x(self):
         return self.rocket_x
@@ -45,11 +45,8 @@ class Rocket:
     def rocket_vel(self):
         return self.rocket_vel
 
-    def hit_points(self):
-        return self.hit_points
-
-    def hit(self):
-        self.hit_points -= 1
+    def rocket_image(self):
+        return self.image
 
 
 player = Rocket()
@@ -75,7 +72,8 @@ laser = Laser()
 
 
 class Rock:
-    def __init__(self, rock_x=600, rock_y=0, rock_vel=5):
+    def __init__(self, rock_x=600, rock_y=0, rock_vel=5, image=rock_rescaled):
+        self.image = image
         self.rock_x = rock_x
         self.rock_y = rock_y
         self.rock_vel = rock_vel
@@ -89,16 +87,37 @@ class Rock:
     def rock_vel(self):
         return self.rock_vel
 
+    def rock_image(self):
+        return self.image
 
-rock = Rock()
+
+def rock_generator(num_of_rocks=5):
+    rock_list = []
+    for rocks in range(num_of_rocks):
+        rand_x = rd.randint(0, width)
+        rock_list.append(rock_rescaled.get_rect(topleft=(rand_x, 0)).copy())
+    return rock_list
 
 
-def main(rocket_y=player.rocket_y, rocket_x=player.rocket_x, rock_y=rock.rock_y, rock_x=rock.rock_x,
-         rock_vel=rock.rock_vel):
+rock1 = Rock(rock_vel=1)
+rock2 = Rock(rock_vel=1)
+rock3 = Rock(rock_vel=1)
+rock4 = Rock(rock_vel=1)
+rock5 = Rock(rock_vel=1)
+rock_array = rock_generator()
+rock_rect1 = rock_array[1]
+rock_rect2 = rock_array[1]
+rock_rect3 = rock_array[2]
+rock_rect4 = rock_array[3]
+rock_rect5 = rock_array[4]
+
+
+def main(rocket_y=player.rocket_y, rocket_x=player.rocket_x, rock1_vel=rock1.rock_vel, rock2_vel=rock2.rock_vel,
+         rock3_vel=rock3.rock_vel, rock4_vel=rock4.rock_vel, rock5_vel=rock5.rock_vel):
     running = True
+    rock_count = 0
     while running:
         pressed_key = pygame.key.get_pressed()
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -110,13 +129,20 @@ def main(rocket_y=player.rocket_y, rocket_x=player.rocket_x, rock_y=rock.rock_y,
             rocket_x += player.rocket_vel
 
         screen.blit(background_rescaled, (0, 0))
-        screen.blit(rock_rescaled, (rock_x, rock_y))
-        screen.blit(rocket_rescaled, (rocket_x, rocket_y))
 
-        if pressed_key[K_0]:
-            while rock_y <= 720:
-                rock_y += rock.rock_vel
-                pygame.display.update()
+        if rock_count < 5:
+            screen.blit(rock1.rock_image(), rock_rect1)
+            screen.blit(rock2.rock_image(), rock_rect2)
+            screen.blit(rock3.rock_image(), rock_rect3)
+            screen.blit(rock4.rock_image(), rock_rect4)
+            screen.blit(rock5.rock_image(), rock_rect5)
+            rock_rect1.move_ip(0, rock1_vel)
+            rock_rect2.move_ip(0, rock2_vel)
+            rock_rect3.move_ip(0, rock3_vel)
+            rock_rect4.move_ip(0, rock4_vel)
+            rock_rect5.move_ip(0, rock5_vel)
+
+        screen.blit(player.rocket_image, (rocket_x, rocket_y))
 
         pygame.display.update()
 
