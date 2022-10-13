a4b91ff5745471b89338a6959fbd9bede8a9a6c8
Daniel Huber <djhuber@iastate.edu>
2022-10-12 20:37:20 -0500

    Falling radnomized rock!

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ center = ((screen.get_width() / 2), 50)
 rocket = pygame.image.load("rocket.png")
 rocket_rescaled = pygame.transform.scale(rocket, (70, 100))
 
-background = pygame.image.load("background.png")
-background_rescaled = pygame.transform.scale(background, (1280, 720))
+background = pygame.image.load("bg.png")
+background_rescaled = pygame.transform.scale(background, (width, height))
 
 laser = pygame.image.load("laser.png")
 laser = pygame.transform.rotate(laser, 90)
@@ -28,95 +28,78 @@ laser_rescaled = pygame.transform.scale(laser, (30, 40))
 rock = pygame.image.load("rock.png")
 rock_rescaled = pygame.transform.scale(rock, (100, 105))
 
+
 class Rocket:
-    def __init__(self, rocket_x=200, rocket_y=550, rocket_vel=10, rocket_image=rocket_rescaled):
-        self.image = rocket_rescaled.get_rect().copy()
-        self.rocket_x = rocket_x
-        self.rocket_y = rocket_y
-        self.rocket_vel = rocket_vel
-        self.rocket_image = rocket_image
+    def __init__(self, rocket_x=200, rocket_y=550, rocket_vel=10):
+        super().__init__()
+        self.x = rocket_x
+        self.y = rocket_y
+        self.width = rocket_rescaled.get_width()
+        self.height = rocket_rescaled.get_height()
+        self.vel = rocket_vel
+        self.image = rocket_rescaled
 
-    def rocket_x(self):
-        return self.rocket_x
-
-    def rocket_y(self):
-        return self.rocket_y
-
-    def rocket_vel(self):
-        return self.rocket_vel
-
-    def rocket_image(self):
-        return self.image
+    def display(self):
+        rect = self.image.get_rect()
+        rect.center = (self.x, self.y)
+        return screen.blit(self.image, (rect.x, rect.y))
 
 
 player = Rocket()
 
 
 class Laser:
-    def __init__(self, laser_x=player.rocket_x, laser_y=player.rocket_y, laser_vel=100):
+    def __init__(self, laser_x=player.x, laser_y=player.y, laser_vel=100):
         self.laser_x = laser_x
         self.laser_y = laser_y
         self.laser_vel = laser_vel
-
-    def laser_x(self):
-        return self.laser_x
-
-    def laser_y(self):
-        return self.laser_y
-
-    def laser_vel(self):
-        return self.laser_vel
 
 
 laser = Laser()
 
 
 class Rock:
-    def __init__(self, rock_x=600, rock_y=0, rock_vel=5, image=rock_rescaled):
+    def __init__(self, rock_y=-100, rock_vel=5, image=rock_rescaled):
+        super().__init__()
         self.image = image
-        self.rock_x = rock_x
-        self.rock_y = rock_y
-        self.rock_vel = rock_vel
+        self.rect = self.image.get_rect()
+        self.x = rd.randint(0, width)
+        self.y = rock_y
+        self.width = rock_rescaled.get_width()
+        self.height = rock_rescaled.get_height()
+        self.vel = rock_vel
 
-    def rock_x(self):
-        return self.rock_x
+    def update(self):
+        self.y += self.vel
+        if self.y >= height + 10:
+            self.y = -100
+            self.x = rd.randint(0, width-70)
 
-    def rock_y(self):
-        return self.rock_y
+    def display(self):
+        rect = self.image.get_rect()
+        rect.center = (self.x, self.y)
+        return screen.blit(self.image, (rect.x, rect.y))
 
-    def rock_vel(self):
-        return self.rock_vel
-
-    def rock_image(self):
-        return self.image
-
-
-def rock_generator(num_of_rocks=5):
-    rock_list = []
-    for rocks in range(num_of_rocks):
-        rand_x = rd.randint(0, width)
-        rock_list.append(rock_rescaled.get_rect(topleft=(rand_x, 0)).copy())
-    return rock_list
+    def hit_box(self):
+        return self.x, self.y, self.width, self.height
 
 
-rock1 = Rock(rock_vel=1)
-rock2 = Rock(rock_vel=1)
-rock3 = Rock(rock_vel=1)
-rock4 = Rock(rock_vel=1)
-rock5 = Rock(rock_vel=1)
-rock_array = rock_generator()
-rock_rect1 = rock_array[1]
-rock_rect2 = rock_array[1]
-rock_rect3 = rock_array[2]
-rock_rect4 = rock_array[3]
-rock_rect5 = rock_array[4]
+objects = []
 
 
-def main(rocket_y=player.rocket_y, rocket_x=player.rocket_x, rock1_vel=rock1.rock_vel, rock2_vel=rock2.rock_vel,
-         rock3_vel=rock3.rock_vel, rock4_vel=rock4.rock_vel, rock5_vel=rock5.rock_vel):
+def rock_generator():
+    for i in range(20):
+        objects.append(Rock())
+
+
+rock_generator()
+
+
+def main():
     running = True
-    rock_count = 0
+    point_count = 0
     while running:
+        clock.tick(60)
         pressed_key = pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -124,25 +107,23 @@ def main(rocket_y=player.rocket_y, rocket_x=player.rocket_x, rock1_vel=rock1.roc
             if pressed_key[K_ESCAPE]:
                 running = False
         if pressed_key[K_LEFT]:
-            rocket_x -= player.rocket_vel
+            if player.x >= 50:
+                player.x -= player.vel
         if pressed_key[K_RIGHT]:
-            rocket_x += player.rocket_vel
+            if player.x <= width - 50:
+                player.x += player.vel
 
         screen.blit(background_rescaled, (0, 0))
+        player.display()
 
-        if rock_count < 5:
-            screen.blit(rock1.rock_image(), rock_rect1)
-            screen.blit(rock2.rock_image(), rock_rect2)
-            screen.blit(rock3.rock_image(), rock_rect3)
-            screen.blit(rock4.rock_image(), rock_rect4)
-            screen.blit(rock5.rock_image(), rock_rect5)
-            rock_rect1.move_ip(0, rock1_vel)
-            rock_rect2.move_ip(0, rock2_vel)
-            rock_rect3.move_ip(0, rock3_vel)
-            rock_rect4.move_ip(0, rock4_vel)
-            rock_rect5.move_ip(0, rock5_vel)
+        for obj in range(len(objects)):
+            objects[obj].display()
+            objects[obj].update()
 
-        screen.blit(player.rocket_image, (rocket_x, rocket_y))
+        if objects[0].y == height + 5:
+            point_count += 1
+
+        pygame.display.set_caption(str(point_count))
 
         pygame.display.update()
 
