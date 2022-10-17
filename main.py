bef07b5c979f342e44a3f2d2f66ba7ed9e20df00
Daniel Huber <djhuber@iastate.edu>
2022-10-16 20:13:54 -0500

    Lasers now explode rocks and point counter for rocks destoryed

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import itertools
 import pygame
 import random as rd
 
@@ -19,6 +20,10 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode([width, height])
 screen_rect = screen.get_rect()
 center = ((screen.get_width() / 2), 50)
+point_font = pygame.font.SysFont('arial', 40)
+txt_font = pygame.font.SysFont('arial', 20)
+
+
 rocket = pygame.image.load("rocket.png")
 rocket_rescaled = pygame.transform.scale(rocket, (70, 100))
 
@@ -32,6 +37,8 @@ laser_rescaled = pygame.transform.scale(laser, (80, 100))
 rock = pygame.image.load("rock.png")
 rock_rescaled = pygame.transform.scale(rock, (70, 80))
 
+point_bar = pygame.image.load("pointbar.png")
+pb_rescaled = pygame.transform.scale(point_bar, (150, 50))
 
 class Rocket:
     def __init__(self, rocket_x=200, rocket_y=600, rocket_vel=10):
@@ -57,13 +64,13 @@ class Rock:
     def __init__(self, rock_y=-200):
         super().__init__()
         self.image = rock_rescaled
-        self.rect = self.image.get_rect()
         self.x = rd.randint(0, width)
         self.y = rock_y
         self.width = rock_rescaled.get_width()
         self.height = rock_rescaled.get_height()
         self.vel = rd.randint(1, 5)
         self.chance = rd.uniform(0, 1)
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
     def update(self):
         self.y += self.vel
@@ -84,11 +91,11 @@ class Laser:
     def __init__(self):
         super().__init__()
         self.image = laser_rescaled
-        self.rect = self.image.get_rect()
         self.x = player.x - 5
         self.y = player.y - 70
         self.width = laser_rescaled.get_width()
         self.height = laser_rescaled.get_height()
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
         self.vel = 40
 
     def display(self):
@@ -108,8 +115,8 @@ def main():
     point_count = 1
     level = 0
     objects = []
-    start_ticks = pygame.time.get_ticks()
     laser_list = []
+    start_ticks = pygame.time.get_ticks()
     time_elapsed_since_last_action = 0
 
     def rock_generator(x):
@@ -151,30 +158,46 @@ def main():
         player.display()
 
         # LASER DISPLAY
-        if pressed_key[K_SPACE] and time_elapsed_since_last_action > 100:
+        if pressed_key[K_SPACE] and time_elapsed_since_last_action > 400:
             laser_shooter()
             time_elapsed_since_last_action = 0
         for obj in range(len(laser_list)):
-            laser_list[obj].display()
-            laser_list[obj].update()
-        for obj in range(len(laser_list)):
-            laser_rect = pygame.Rect(laser_list[obj].x, laser_list[obj].y, laser_list[obj].width, laser_list[obj].height)
-            rock_rect = pygame.Rect(objects[obj].x, objects[obj].y, objects[obj].width, objects[obj].height)
-            if pygame.Rect.colliderect(rock_rect, laser_rect):
-                objects[obj].pop
+                laser_list[obj].display()
+                laser_list[obj].update()
+                if laser_list[obj].y < -100:
+                    laser_list.pop(obj)
 
+        # HIT DETECTION FOR LASERS AND ROCKS and point counter
+        for rock in objects[:]:
+            for laser in laser_list[:]:
+                if pygame.sprite.collide_rect(laser, rock):
+                    objects.remove(rock)
+                    laser_list.remove(laser)
+                    point_count += 1
 
         # LEVELER MORE ROCKS EVERY 10 SECONDS
         if seconds >= 10 and level == 1:
-            rock_count = 13
+            rock_count = 20
             rock_generator(rock_count)
             level += 1
         elif seconds >= 20 and level == 2:
-            rock_count = 16
+            rock_count = 40
             rock_generator(rock_count)
             level += 1
         elif seconds >= 30 and level == 3:
-            rock_count = 20
+            rock_count = 60
+            rock_generator(rock_count)
+            level += 1
+        elif seconds >= 40 and level == 4:
+            rock_count = 100
+            rock_generator(rock_count)
+            level += 1
+        elif seconds >= 50 and level == 5:
+            rock_count = 140
+            rock_generator(rock_count)
+            level += 1
+        elif seconds >= 60 and level == 6:
+            rock_count = 160
             rock_generator(rock_count)
             level += 1
 
@@ -183,19 +206,21 @@ def main():
             for obj in range(len(objects)):
                 objects[obj].display()
                 objects[obj].update()
-        if level >= 1:
-            for obj in range(len(objects)):
-                if objects[obj].y == height + 5:
-                    point_count += 1
 
-        # HIT DETECTION
+        # HIT DETECTION FOR PLAYER AND ROCKS
         for obj in range(len(objects)):
             rock_rect = pygame.Rect(objects[obj].x, objects[obj].y, objects[obj].width, objects[obj].height)
             player_rect = pygame.Rect(player.x + 10, player.y + 10, player.width - 10, player.height - 10)
             if pygame.Rect.colliderect(rock_rect, player_rect):
                 running = False
 
-        pygame.display.set_caption("POINT COUNT " + str(point_count))
+        # Below is rendering my point box with points
+        screen.blit(pb_rescaled, (width-162, 660))
+        points = point_font.render(str(point_count), True, (255, 255, 255))
+        total_points_txt = txt_font.render("POINTS:", True, (255, 255, 255))
+        screen.blit(total_points_txt, (width - 150, 672))
+        screen.blit(points, (width-70, 662))
+        pygame.display.set_caption("ROCKETS")
         pygame.display.update()
 
 
